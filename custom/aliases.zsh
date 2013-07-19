f01d09cb0d175824465eca160fa72950ec788298
Larry Lv <larrylv1990@gmail.com>
2013-07-19 12:14:21 +0800

    Add aliases for quick cd into code bases.

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -9,3 +9,5 @@ alias be='bundle exec'
 alias pt='pstree'
 alias bi='bundle install --path=vendor/bundle --binstubs=.binstubs'
 alias gi='gem install'
+u() { cd ~/Code/Umeng/"$*" }
+h() { cd ~/Code/Hacks/"$*" }
