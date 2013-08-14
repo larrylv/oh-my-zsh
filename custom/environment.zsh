2afb55bd9e3dd1ba37894ceab2e3fef0164482d1
Larry Lv <larrylv1990@gmail.com>
2013-08-14 14:05:54 +0800

    Tab completion for bundler binstubs.

--- custom/environment.zsh
+++ custom/environment.zsh
@@ -20,3 +20,6 @@ export EDITOR=vim
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+
+# Complete $PWD/.binstubs if it exists. Good for bundler with binstubs
+zstyle -e ':completion:*' command-path '[[ -d $PWD/.binstubs ]] && reply=($PWD/.binstubs $path)'
