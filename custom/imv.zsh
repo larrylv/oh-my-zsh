0444f5c3c2f6db69ad6de60387ab9d703442a56c
Larry Lv <larrylv1990@gmail.com>
2013-09-08 23:23:04 +0800

    Add imv function.

---
+++ custom/imv.zsh
@@ -0,0 +1,9 @@
+imv() {
+  local src dst
+  for src; do
+    [[ -e $src ]] || { print -u2 "$src does not exist"; continue }
+    dst=$src
+    vared dst
+    [[ $src != $dst ]] && mkdir -p $dst:h && mv -n $src $dst
+  done
+}
