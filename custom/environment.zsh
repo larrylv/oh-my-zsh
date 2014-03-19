bb296f8a4be0f499e6dfc0fa982da41a0aa30000
Larry Lv <larrylv1990@gmail.com>
2014-03-19 15:17:23 +0800

    Remove PATH setting from oh-my-zsh.

--- custom/environment.zsh
+++ custom/environment.zsh
@@ -5,19 +5,6 @@ HISTFILE=~/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=HISTSIZE
 
-# MacPorts is uninstalled, so no need to add `/opt/local/bin` to $PATH
-# [[ -s "/opt/local/bin" ]] && export PATH=/opt/local/bin:$PATH
-[[ -s "/usr/local/bin" ]] && export PATH=/usr/local/bin:$PATH
-[[ -s "/usr/local/sbin" ]] && export PATH=/usr/local/sbin:$PATH
-
-# I dropped rvm a few days ago, so shell won't add append BUNDLE_BIN path to the
-# PATH when changing directories. And I always use `--binstubs=.binstubs`
-# option for bundler. So it will be great to add `.binstubs` to the PATH.
-# Thank god I won't type `bundle exec` or its alias `be` any more.
-export PATH=.binstubs:$PATH
-
-export PATH=$PATH:/usr/local/Cellar/go/1.2/libexec/bin
-
 export EDITOR=vim
 
 export LC_ALL=en_US.UTF-8
